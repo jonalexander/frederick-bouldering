1858ed50be6413c7f863c35abad0d00b0c6dcde9
Jonathan Alexander <jonathandalexander@gmail.com>
2016-05-02 20:16:35 -0400

    s3 config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,6 +49,7 @@ Rails.application.configure do
   # Configurations for Paperclip to upload to AWS S3
   config.paperclip_defaults = {
   :storage => :s3,
+  :s3_region => ENV['AWS_REGION'],
   :s3_credentials => {
     :bucket => ENV['S3_BUCKET_NAME'],
     :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
